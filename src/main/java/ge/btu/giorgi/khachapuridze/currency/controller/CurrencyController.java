53321d0373b4af287b2824029a3514eaa212a885
Gkhachapuridze <giorgi.khatchapuridze.1@gmail.com>
2020-04-08 18:55:16 +0400

    init assignment one

---
+++ src/main/java/ge/btu/giorgi/khachapuridze/currency/controller/CurrencyController.java
@@ -0,0 +1,42 @@
+package ge.btu.giorgi.khachapuridze.currency.controller;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import java.util.ArrayList;
+import java.util.List;
+import ge.btu.giorgi.khachapuridze.currency.model.Currency;
+@Path("/currencyManager")
+public class CurrencyController {
+
+    @GET
+    @Path("/currencies")
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Currency> getCurrency() {
+        List<Currency> currencyList=new ArrayList<>();
+        currencyList.add(new Currency("EUR",3.16,3.2));
+        currencyList.add(new Currency("USD",3.43,3.475));
+        currencyList.add(new Currency("EUR",4.5,3.1));
+        currencyList.add(new Currency("AZN",4.5,3.1));
+        currencyList.add(new Currency("TRY",4.5,3.1));
+        currencyList.add(new Currency("RUB",4.5,3.1));
+        return currencyList;
+    }
+
+    @POST
+    @Path("currency/buy")
+    @Produces(MediaType.APPLICATION_JSON) // აბრუნებს ჯეისონს
+    @Consumes(MediaType.APPLICATION_JSON) // იღებს ჯეისონს
+    public void addCurrency(Currency currency) {
+        System.out.println(currency.toString());
+    }
+
+
+    @PUT
+    @Path("/currency/sell")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Currency updateCurrency(Currency currency) {
+        System.out.println(currency.toString());
+        return currency;
+    }
+}
+
---
+++ src/main/java/ge/btu/giorgi/khachapuridze/currency/model/Currency.java
@@ -0,0 +1,52 @@
+package ge.btu.giorgi.khachapuridze.currency.model;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class Currency {
+
+    private String title;
+    private double buy;
+    private double sell;
+
+    public Currency() {
+    }
+
+    public Currency(String title, double buy, double sell) {
+        this.title = title;
+        this.buy = buy;
+        this.sell = sell;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public double getBuy() {
+        return buy;
+    }
+
+    public void setBuy(double buy) {
+        this.buy = buy;
+    }
+
+    public double getSell() {
+        return sell;
+    }
+
+    public void setSell(double sell) {
+        this.sell = sell;
+    }
+
+    @Override
+    public String toString() {
+        return "Currency{" +
+                "title='" + title + '\'' +
+                ", buy=" + buy +
+                ", sell=" + sell +
+                '}';
+    }
+}
